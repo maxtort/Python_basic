9bd86b3c8dfaec64f413f68f57e414e0e4e447ae
maxtort <topormaks3217@gmail.com>
2024-07-15 14:23:58 -0700

    10 lesson

--- lesson_08_07.py
+++ lesson_08_07.py
@@ -32,3 +32,30 @@ import string
 # print('OK')
 
 
+# # # # # # # # # # # # # # # # # # Генераторная функция
+
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+
+    yield begin
+
+
+    for num in range(1, end):
+        begin = func(begin)
+        yield begin
+
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
